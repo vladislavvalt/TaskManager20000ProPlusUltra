8bd6ddea0e36722c4779d4c4c6ad99f7548bdb6d
Danylo Fitel <danylo.fitel@outlook.com>
2014-04-21 17:47:54 +0300

    Employee controller assembles current user by saving ApplicationUser and Employee entities in the ViewBag.

--- TaskManager20000ProPlusUltra/TaskManager20000ProPlusUltra/Controllers/EmployeeController.cs
+++ TaskManager20000ProPlusUltra/TaskManager20000ProPlusUltra/Controllers/EmployeeController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using TaskManager20000ProPlusUltra.Models;
 using TaskManager20000ProPlusUltra.TaskManagerDatabase;
 
 namespace TaskManager20000ProPlusUltra.Controllers
@@ -15,29 +18,59 @@ namespace TaskManager20000ProPlusUltra.Controllers
         // GET: /Employee/
         public ActionResult Index()
         {
-            ViewBag.Employee = LoggedInEmployee();
+            ViewBag.User = LoggedInUser();
+            ViewBag.Employee = GetEmployee(ViewBag.User);
             return View();
         }
 
         public ActionResult Tasks()
         {
-            var employee = LoggedInEmployee();
+            var employee = LoggedInUser();
             IEnumerable<Task> tasks;
+
             using (var context = new CompanyContext())
             {
                 tasks = from task in context.Tasks
-                    where task.EmployeeId == employee.EmployeeId
+                    where task.EmployeeId == employee.Id
                     select task;
                 tasks = tasks.ToList();
             }
+
             return View(tasks);
         }
 
-        private Employee LoggedInEmployee()
+        private ApplicationUser LoggedInUser()
         {
-            Employee employee = new Employee();
-            employee.EmployeeId = "4";
-            return employee;
+            var userId = User.Identity.GetUserId();
+            using (var context = new CompanyContext())
+            {
+                var users = from u in context.Users
+                    where u.Id == userId
+                    select u;
+                foreach (var u in users)
+                {
+                    return u;
+                }
+            }
+
+            return null;
+        }
+
+        private Employee GetEmployee(ApplicationUser user)
+        {
+            using (var context = new CompanyContext())
+            {
+                var employees = from employee in context.Employees
+                    where employee.EmployeeId == user.Id
+                    select employee;
+
+                foreach (var e in employees)
+                {
+                    return e;
+                }
+            }
+
+            return null;
         }
 	}
 }
